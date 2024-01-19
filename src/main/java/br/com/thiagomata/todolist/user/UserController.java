2e05582c3fbabc7cbd97dc0fa92027eee7db85b3
thiago.mata@agenciam3.com <taiagomata@gmail.com>
2024-01-18 21:52:15 -0300

    feat: criado banco h2 e adicionado jpa

--- src/main/java/br/com/thiagomata/todolist/user/UserController.java
+++ src/main/java/br/com/thiagomata/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.thiagomata.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired // Used to inject the dependency
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.username);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("User already exists");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
